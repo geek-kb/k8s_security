21bebcd207bb4f76955301ba19ec64664bc89d76
Itai Ganot <lel@lel.bz>
2025-03-10 15:24:39 +0200

    Update site settings

--- sidebars.ts
+++ sidebars.ts
@@ -1,18 +1,19 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
+  // The 'Introduction' doc as a standalone menu item
   tutorialSidebar: [
+    {
+      type: 'doc',
+      id: 'intro', // The document ID matches the 'intro.md' file
+      label: 'Introduction',
+    },
     {
       type: 'category',
       label: 'K8s Security Basics',
       collapsible: true,
       collapsed: false,
       items: [
-        {
-          type: 'doc',
-          id: 'intro',
-          label: 'Introduction',
-        },
         'k8s-security-basics/understanging_k8s_attack_surface',
         'k8s-security-basics/the_4_c_cloud_native_security',
       ],
