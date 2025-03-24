681e3b312c18dad3396ac9e0db2e8cb193f143ba
Itai Ganot <lel@lel.bz>
2025-03-24 14:38:22 +0200

    Update root redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
+import React, { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/docs/intro" />;
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push('/docs/intro'); // Update the path to match your site's structure
+  }, [history]);
+
+  return null;
 }
+
