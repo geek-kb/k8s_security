77375bc8459e658a9f3100b77535bae61e30866b
Itai Ganot <lel@lel.bz>
2025-03-24 21:48:11 +0200

    Update site content

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,17 +66,17 @@ const config: Config = {
     ],
   ],
 
-  headTags: [
-    {
-      tagName: 'script',
-      attributes: {}, // Required even if empty
-      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KD86KG');`,
-    },
-  ],  
+  // headTags: [
+  //   {
+  //     tagName: 'script',
+  //     attributes: {}, // Required even if empty
+  //     innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  //     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  //     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  //     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  //     })(window,document,'script','dataLayer','GTM-KD86KG');`,
+  //   },
+  // ],  
 
   themeConfig: {
     navbar: {
